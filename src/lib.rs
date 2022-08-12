1cfcb8b4ea15f79b55e4f857ff8c28c5081b7aa7
MuZhou233 <MuZhou233@users.noreply.github.com>
2022-08-12 18:29:23 +0000

    Apply automatic changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 // @generated
-#[cfg(feature = "librarian")]
-// @@protoc_insertion_point(attribute:librarian)
 pub mod librarian {
-    include!("librarian.rs");
-    // @@protoc_insertion_point(librarian)
     pub mod mapper {
         #[cfg(feature = "librarian-mapper-v1")]
         // @@protoc_insertion_point(attribute:librarian.mapper.v1)
@@ -12,10 +8,4 @@ pub mod librarian {
             // @@protoc_insertion_point(librarian.mapper.v1)
         }
     }
-}
-#[cfg(feature = "sentinel")]
-// @@protoc_insertion_point(attribute:sentinel)
-pub mod sentinel {
-    include!("sentinel.rs");
-    // @@protoc_insertion_point(sentinel)
 }
