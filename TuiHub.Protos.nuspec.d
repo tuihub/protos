254b95a3b3b1ccaedd41add470336beab89a0491
Tabing010102 <tabing010102@outlook.com>
2022-09-21 11:31:52 +0800

    update c# config

---
+++ TuiHub.Protos.nuspec.d
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<package>
+  <metadata>
+    <id>TuiHub.Protos</id>
+    <version>0.1.0</version>
+    <authors>TuiHub</authors>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <license type="expression">MIT</license>
+    <!-- <icon>icon.png</icon> -->
+    <projectUrl>https://github.com/TuiHub/Protos</projectUrl>
+    <repository type="git" url="https://github.com/TuiHub/Protos" />
+    <description>protobuf protocol files for TuiHub</description>
+    <releaseNotes></releaseNotes>
+    <copyright></copyright>
+    <tags></tags>
+  </metadata>
+  <files>
+      <file src="TuiHub.Protos.csproj" target="" />
+      <file src="README.md" target="" />
+      
+      <file src="Assets\**\*.*" target="" />
+  </files>
+</package>
