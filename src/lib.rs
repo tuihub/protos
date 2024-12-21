f505daf450e80e6544fbedd89f3e2d5c55c7941b
MuZhou233 <MuZhou233@users.noreply.github.com>
2024-12-21 19:10:14 +0000

    chore: apply auto code generation

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,6 @@ pub mod errors {
     // @@protoc_insertion_point(errors)
 }
 pub mod librarian {
-    pub mod mapper {
-        #[cfg(feature = "librarian-mapper-v1")]
-        // @@protoc_insertion_point(attribute:librarian.mapper.v1)
-        pub mod v1 {
-            include!("librarian.mapper.v1.rs");
-            // @@protoc_insertion_point(librarian.mapper.v1)
-        }
-    }
     pub mod miner {
         #[cfg(feature = "librarian-miner-v1")]
         // @@protoc_insertion_point(attribute:librarian.miner.v1)
@@ -42,14 +34,6 @@ pub mod librarian {
         pub mod v1 {
             include!("librarian.porter.v1.rs");
             // @@protoc_insertion_point(librarian.porter.v1)
-        }
-    }
-    pub mod searcher {
-        #[cfg(feature = "librarian-searcher-v1")]
-        // @@protoc_insertion_point(attribute:librarian.searcher.v1)
-        pub mod v1 {
-            include!("librarian.searcher.v1.rs");
-            // @@protoc_insertion_point(librarian.searcher.v1)
         }
     }
     pub mod sephirah {
